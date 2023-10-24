a70de324a69fd7b4b3c313dd1a512e8683398098
Lebene Gbebleou-Sleem <ChantelleSleem@gmail.com>
2023-10-24 11:15:03 -0700

    errors fixed

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,16 +1,15 @@
---a script that prepares a MySQL server for the project
--- Define variables
+-- a script that prepares a MySQL server for the project
 -- Create the database if it doesn't exist:hbnb_test_db
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 
 -- Create the user if it doesn't exist and set the password
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 
--- Grant all privileges on the database
-GRANT ALL PRIVILEGES ON 'hbnb_test_db'.* TO 'hbnb_test'@'localhost';
-
 -- Grant SELECT privilege on the performance_schema database
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+
+-- Grant all privileges on the database
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 
 -- Flush privileges to apply changes
 FLUSH PRIVILEGES;
