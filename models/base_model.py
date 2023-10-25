d91f6d9b1a541ce6304ee7a0734bff059d8421d9
Akos-1 <veronicagyasi@gmail.com>
2023-10-25 14:46:27 -0700

    update

--- models/base_model.py
+++ models/base_model.py
@@ -5,7 +5,6 @@ from sqlalchemy import Column, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from datetime import datetime
 import uuid
-from models import storage
 
 
 Base = declarative_base()
@@ -21,6 +20,7 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
+        from models import storage
         if not kwargs:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
